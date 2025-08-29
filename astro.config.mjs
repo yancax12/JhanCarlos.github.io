386f8503649c3a3a39a62b088513ea65ae085842
Jhan Carlos Yangali Huaman <yangaliyanca@gmail.com>
2025-08-28 21:55:42 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://neonmint.efeele.dev",
+  site: "https://JhanCarlos.github.io",
   integrations: [preact(), icon(), sitemap({
     filter: (page) =>
       !page.includes("/blog/tags") &&
