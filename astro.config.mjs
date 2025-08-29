808f19fb35f5e291229c2d2801b68fe1b2b355b5
Jhan Carlos Yangali Huaman <yangaliyanca@gmail.com>
2025-08-29 03:14:31 +0000

    Add deploy configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,28 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import preact from "@astrojs/preact";
-import sitemap from "@astrojs/sitemap"
+import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://yancax12.github.io/JhanCarlos.github.io/",
-
-  integrations: [preact(), icon(), sitemap({
-    filter: (page) =>
-      !page.includes("/blog/tags") &&
-      !page.includes("/blog/techs"),
-  }),],
-
+  base: "/JhanCarlos.github.io/",   // 👈 ESTA ES LA CLAVE
+  integrations: [
+    preact(),
+    icon(),
+    sitemap({
+      filter: (page) =>
+        !page.includes("/blog/tags") &&
+        !page.includes("/blog/techs"),
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
   markdown: {
     shikiConfig: {
       theme: 'github-dark'
-    },
-  },
+    }
+  }
 });
